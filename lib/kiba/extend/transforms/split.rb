8d8fde13c9b017df6a97a8d54d88ca1c0088d8d8
Kristina Spurgin <kristina.spurgin@lyrasis.org>
2020-06-30 17:16:06 -0400

    add option to collapse left or right to Split::IntoMultipleColumns

--- lib/kiba/extend/transforms/split.rb
+++ lib/kiba/extend/transforms/split.rb
@@ -11,41 +11,81 @@ module Kiba
         # max_segments -- integer -- default: 9999 (represents unlimited) -- optionally specifify maximum number
         #   of segments to split into (i.e. max number of columns to create from this one column). Values farther
         #   to the right will be joined back together and included in final column
+        # collapse_on -- symbol -- default: :right -- on which end of the split array to join remaining split values
+        #   if there are more than max_segments
         # warnfield -- symbol -- default: nil -- fieldname in which to put warning/error(s) for a row
         class IntoMultipleColumns
-          def initialize(field:, sep:, delete_source: true, max_segments: 9999, warnfield: nil)
+          def initialize(field:, sep:, delete_source: true, max_segments: 9999, collapse_on: :right, warnfield: nil)
             @field = field
             @sep = sep
             @del = delete_source
             @max = max_segments
+            @collapse_on = collapse_on
             @warnfield = warnfield
           end
 
           def process(row)
             val = row.fetch(@field, nil)
             if val
-              segments_remaining = []
-              final_index = 0
-              val.split(@sep).each_with_index do |v, i|
-                if i < @max - 1
-                  v = v.strip.empty? ? nil : v.strip
-                  row["#{@field}#{i}".to_sym] = v
-                  final_index = i
-                else
-                  segments_remaining << v
+
+              valsplit = val.split(@sep)
+
+              if valsplit.size > @max && @warnfield
+                row[@warnfield] = 'max_segments less than total number of split segments'
+              elsif valsplit.size <= @max && @warnfield
+                row[@warnfield] = nil
+              end
+
+              if valsplit.size <= @max
+                process_splits(valsplit, row)
+              else
+                case @collapse_on
+                when :right
+                  process_splits(valsplit, row)
+                when :left
+                  process_left_split(valsplit, row)
                 end
               end
-              unless segments_remaining.empty?
-                val = segments_remaining.join(@sep)
-                row["#{@field}#{final_index + 1}".to_sym] = val.strip
-                unless @warnfield.nil?
-                  row[@warnfield] = 'max_segments less than total number of split segments'
-                end
-              end
-              
               row.delete(@field) if @del
             end
             row
+          end
+
+          private
+
+          def process_left_split(valsplit, row)
+            diff = valsplit.size - @max
+            leftside = []
+            leftside << valsplit.shift until valsplit.size == diff
+            row["#{@field}0".to_sym] = leftside.join(@sep).strip
+            valsplit.each_with_index do |v, i|
+              v = v.strip.empty? ? nil : v.strip
+              row["#{@field}#{i + 1}".to_sym] = v
+            end
+          end
+          
+          def process_splits(valsplit, row)
+            segments_remaining = []
+            final_index = 0
+            valsplit.each_with_index do |v, i|
+              if i < @max - 1
+                v = v.strip.empty? ? nil : v.strip
+                row["#{@field}#{i}".to_sym] = v
+                final_index = i
+              else
+                segments_remaining << v
+              end
+            end
+
+            unless segments_remaining.empty?
+              if segments_remaining.size == 1
+                row["#{@field}#{final_index + 1}".to_sym] = segments_remaining[0].strip
+              else
+                val = segments_remaining.join(@sep)
+                row["#{@field}#{final_index + 1}".to_sym] = val.strip
+              end
+            end
+
           end
         end
       end
--- spec/kiba/extend/transforms/split_spec.rb
+++ spec/kiba/extend/transforms/split_spec.rb
@@ -50,6 +50,7 @@ RSpec.describe Kiba::Extend::Transforms::Split do
 
       context 'and max_segments = 2' do
         context 'and value = overall: 8x10 ; base: 7x7' do
+          context 'and collapse_on = :right' do
           context 'and no warnfield given' do
             rows = [
               ['id', 'summary'],
@@ -97,6 +98,61 @@ RSpec.describe Kiba::Extend::Transforms::Split do
                                      warnfield: :warn
                                    })
               expect(result).to eq(expected)
+            end
+          end
+          end
+
+          context 'and collapse_on = :left' do
+            context 'and no warnfield given' do
+              rows = [
+                ['id', 'summary'],
+                [1, 'overall: 8x10; base: 7x7']
+              ]
+              @info = []
+              before do
+                generate_csv(test_csv, rows)
+              end
+              it 'splits into two columns' do
+                expected = [
+                  {id: '1', summary0: 'overall: 8x10; base', summary1: '7x7'}
+                ]
+                result = execute_job(filename: test_csv,
+                                     xform: Split::IntoMultipleColumns,
+                                     xformopt: {
+                                       field: :summary,
+                                       sep: ':',
+                                       max_segments: 2,
+                                       collapse_on: :left
+                                     })
+                expect(result).to eq(expected)
+              end
+            end
+          end
+
+          context 'and value = overall: 8x10 ; 7x7' do
+            context 'and warnfield given' do
+              rows = [
+                ['id', 'summary'],
+                [1, 'overall: 8x10 ; 7x7']
+              ]
+              @info = []
+              before do
+                generate_csv(test_csv, rows)
+              end
+              it 'splits into two columns' do
+                expected = [
+                  {id: '1', summary0: 'overall', summary1: '8x10 ; 7x7', warn: nil}
+                ]
+                result = execute_job(filename: test_csv,
+                                     xform: Split::IntoMultipleColumns,
+                                     xformopt: {
+                                       field: :summary,
+                                       sep: ':',
+                                       max_segments: 2,
+                                       warnfield: :warn
+                                     })
+                expect(result).to eq(expected)
+              end
             end
           end
         end
