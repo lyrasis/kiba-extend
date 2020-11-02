437a3c657ad11f94651c2d7974de170ead8fe8c3
Kristina Spurgin <kristina.spurgin@lyrasis.org>
2020-11-02 14:02:54 -0500

    Replace::FieldValueWithStaticMapping can handle multival fields

--- lib/kiba/extend/transforms/replace.rb
+++ lib/kiba/extend/transforms/replace.rb
@@ -4,28 +4,37 @@ module Kiba
       module Replace
         ::Replace = Kiba::Extend::Transforms::Replace
         class FieldValueWithStaticMapping
-          def initialize(source:, target:, mapping:, fallback_val: :orig, delete_source: true)
+          def initialize(source:, target:, mapping:, fallback_val: :orig, delete_source: true,
+                         multival: false, sep: '')
             @source = source
             @target = target
             @mapping = mapping
             @mapping[nil] = nil unless @mapping.has_key?(nil)
             @fallback = fallback_val
             @del = delete_source
+            @multival = multival
+            @sep = sep
           end
 
           def process(row)
-            origval = row.fetch(@source)
-            if @mapping.has_key?(origval)
-              row[@target] = @mapping[origval]
-            else
-              case @fallback
+            origval = @multival ? row.fetch(@source).split(@sep) : [row.fetch(@source)]
+            newvals = []
+            
+            origval.each do |oval|
+              if @mapping.has_key?(oval)
+                newvals << @mapping[oval]
+              else
+                case @fallback
                 when :orig
-                  row[@target] = origval
-              when :nil
-                row[@target] = nil
+                  newvals << oval
+                when :nil
+                  newvals << nil
+                end
               end
             end
-            row.delete(@source) if @del
+
+            row[@target] =  newvals.length > 1 ? newvals.join(@sep) : newvals.first
+            row.delete(@source) if @del unless @source == @target
             row
           end
         end
