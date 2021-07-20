0efcada16b096d57a1d2bd1ba57a881f6813bf82
Kristina Spurgin <kristina.spurgin@lyrasis.org>
2021-07-20 11:39:31 -0400

    add YARD documentation

--- lib/kiba/extend/transforms/split.rb
+++ lib/kiba/extend/transforms/split.rb
@@ -1,20 +1,80 @@
 module Kiba
   module Extend
     module Transforms
-      module Split
+      # Transformations that split data
+      module Split 
         ::Split = Kiba::Extend::Transforms::Split
 
         # Splits field into multiple fields, based on sep
-        # New columns will use original field name, and add number to end (:field0, :field1, etc)
-        # sep -- string -- string on which to split
-        # delete_source -- boolean -- default: true -- If true, removes original field. If false, leaves original field
-        # max_segments -- integer -- default: 9999 (represents unlimited) -- optionally specifify maximum number
-        #   of segments to split into (i.e. max number of columns to create from this one column). Values farther
-        #   to the right will be joined back together and included in final column
-        # collapse_on -- symbol -- default: :right -- on which end of the split array to join remaining split values
-        #   if there are more than max_segments
-        # warnfield -- symbol -- default: nil -- fieldname in which to put warning/error(s) for a row
+        # New columns use the original field name, and add number to end (:field0, :field1, etc)
+        #
+        # # Examples
+        #
+        # Input table:
+        #
+        # ```
+        # | summary                   |
+        # |---------------------------|
+        # | overall: 8x10; 5x7        |
+        # | : 8x10; 5x7               |
+        # | overall: 8x10; base: 7x7  |
+        # ```
+        #
+        # Used in pipeline as:
+        #
+        # ```
+        # transform Split::IntoMultipleColumns, field: :summary, sep: ':'
+        # ```
+        #
+        # Results in:
+        #
+        # ```
+        # | summary0 | summary1   | summary2 |
+        # |----------+------------+----------|
+        # | overall  | 8x10; 5x7  |          |
+        # | nil      | 8x10; 5x7  |          |
+        # | overall  | 8x10; base |      7x7 |
+        # ```
+        #
+        # Used in pipeline as:
+        #
+        # ```
+        # transform Split::IntoMultipleColumns, field: :summary, sep: ':', max_segments: 2
+        # ```
+        #
+        # Results in:
+        #
+        # ```
+        # | summary0 | summary1        |
+        # |----------+-----------------|
+        # | overall  | 8x10; 5x7       |
+        # | nil      | 8x10; 5x7       |
+        # | overall  | 8x10; base: 7x7 |
+        # ```
+        #
+        # Used in pipeline as:
+        #
+        # ```
+        # transform Split::IntoMultipleColumns, field: :summary, sep: ':', max_segments: 2,
+        #   collapse_on: :left, warnfield: :warnme
+        # ```
+        #
+        # Results in:
+        #
+        # ```
+        # | summary0            | summary1  | warnme                                                |
+        # |---------------------+-----------+-------------------------------------------------------|
+        # | overall             | 8x10; 5x7 |                                                       |
+        # | nil                 | 8x10; 5x7 |                                                       |
+        # | overall: 8x10; base | 7x7       | max_segments less than total number of split segments |
+        # ```
         class IntoMultipleColumns
+          # @param field [Symbol] Name of field to split
+          # @param sep [String] Character(s) on which to split the field value
+          # @param delete_source [Boolean] Whether to delete `field` after splitting it into new columns
+          # @param max_segments [Integer] Optional specification of the maximum number of segments to split `field` value into (i.e. max number of columns to create from this one column).
+          # @param collapse_on [:right, :left] Which end of the split array to join remaining split values if there are more than max_segments
+          # @param warnfield [Symbol] Name of field in which to put any warning/error(s) for a row
           def initialize(field:, sep:, delete_source: true, max_segments: 9999, collapse_on: :right, warnfield: nil)
             @field = field
             @sep = sep
@@ -24,6 +84,7 @@ module Kiba
             @warnfield = warnfield
           end
 
+          # @private
           def process(row)
             val = row.fetch(@field, nil)
             if val
