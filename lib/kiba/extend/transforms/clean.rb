16018170e6f4dc8b4882a24cbeb3ee06a830cb93
Kristina Spurgin <kristina.spurgin@lyrasis.org>
2021-07-21 18:34:38 -0400

    Clean::Helpers.delim_only? bugfix for nil values; add tests, docs

--- lib/kiba/extend/transforms/clean.rb
+++ lib/kiba/extend/transforms/clean.rb
@@ -3,14 +3,28 @@
 module Kiba
   module Extend
     module Transforms
+      # Transformations to clean up data
       module Clean
         ::Clean = Kiba::Extend::Transforms::Clean
 
+        # Mixins for Clean transformations
         module Helpers
+          extend self
+          # Indicates whether a field value is delimiter-only. If `usenull` is set to true, the
+          #   %NULLVALUE% string is treated as empty in detecting delimiter-only-ness
+          # @param val [String] The field value to check
+          # @param delim [String] The multivalue delimiter
+          # @param usenull [Boolean] If true, replaces '%NULLVALUE%' with '' to make determination
+          # @return [false] if `value` is nil, empty, or contains characters other than delimiter(s)
+          #   and leading/trailing spaces
+          # @return [true] if `value` contains only delimiter(s) and leading/trailing spaces
           def delim_only?(val, delim, usenull = false)
+            return false if val.nil?
+            return false if val.strip.empty?
+            
             chk = val.gsub(delim, '').strip
             chk = chk.gsub('%NULLVALUE%', '').strip if usenull
-            chk.empty? ? true : false
+            chk.empty?
           end
         end
 
--- spec/kiba/extend/transforms/clean_spec.rb
+++ spec/kiba/extend/transforms/clean_spec.rb
@@ -3,6 +3,86 @@
 require 'spec_helper'
 
 RSpec.describe Kiba::Extend::Transforms::Clean do
+  describe 'Helpers' do
+    describe '#delim_only?' do
+      context 'when delim = |' do
+        let(:delim) { '|' }
+        let(:usenull) { false }
+        let(:result) { Clean::Helpers.delim_only?(value, delim, usenull) }
+        context 'when value = foo|bar' do
+          let(:value) { 'foo|bar' }
+          it 'returns false' do
+            expect(result).to be false
+          end
+        end
+        context 'when value = |' do
+          let(:value) { '|' }
+          it 'returns true' do
+            expect(result).to be true
+          end
+        end
+        context 'when value = %NULLVALUE%|%NULLVALUE%' do
+          let(:value) { '%NULLVALUE%|%NULLVALUE%' }
+          it 'returns false' do
+            expect(result).to be false
+          end
+        end
+        context 'when value = %NULLVALUE%' do
+          let(:value) { '%NULLVALUE%' }
+          it 'returns false' do
+            expect(result).to be false
+          end
+        end
+        context 'when value = %NULLVALUE%|blah' do
+          let(:value) { '%NULLVALUE%|blah' }
+          it 'returns false' do
+            expect(result).to be false
+          end
+        end
+        context 'when value = blank' do
+          let(:value) { '' }
+          it 'returns false' do
+            expect(result).to be false
+          end
+        end
+        context 'when value = nil' do
+          let(:value) { nil }
+          it 'returns false' do
+            expect(result).to be false
+          end
+        end
+        context 'when value = " "' do
+          let(:value) { ' ' }
+          it 'returns false' do
+            expect(result).to be false
+          end
+        end
+
+        context 'when usenull = true' do
+          let(:usenull) { true }
+          context 'when value = %NULLVALUE%|%NULLVALUE%' do
+            let(:value) { '%NULLVALUE%|%NULLVALUE%' }
+            it 'returns true' do
+              expect(result).to be true
+            end
+          end
+          context 'when value = %NULLVALUE%' do
+            let(:value) { '%NULLVALUE%' }
+            it 'returns true' do
+              expect(result).to be true
+            end
+          end
+          context 'when value = %NULLVALUE%|blah' do
+            let(:value) { '%NULLVALUE%|blah' }
+            it 'returns false' do
+              expect(result).to be false
+            end
+          end
+        end
+      end
+    end
+  end
+
   describe 'AlphabetizeFieldValues' do
     test_csv = 'tmp/test.csv'
     rows = [
@@ -60,13 +140,6 @@ RSpec.describe Kiba::Extend::Transforms::Clean do
         result = execute_job(filename: test_csv,
                              xform: Clean::ClearFields,
                              xformopt: { fields: %i[type], if_equals: ';' })
-        expected = [
-          { id: '1', type: 'Person;unmapped;Organization' },
-          { id: '2', type: nil },
-          { id: '3', type: nil },
-          { id: '4', type: '' },
-          { id: '5', type: 'Person;notmapped' }
-        ]
         expect(result.map { |e| e[1] }.uniq[0]).to be_nil
       end
     end
