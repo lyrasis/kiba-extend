7150a071c033edf6c11f4836e25b4793265cd206
Kristina Spurgin <kristina.spurgin@lyrasis.org>
2021-10-04 18:38:21 -0400

    add multival processing to `Cspace::NormalizeForID` transform

--- lib/kiba/extend/transforms/cspace.rb
+++ lib/kiba/extend/transforms/cspace.rb
@@ -49,24 +49,35 @@ module Kiba
         end
 
         class NormalizeForID
-          def initialize(source:, target:)
+          def initialize(source:, target:, multival: false, delim: nil)
             @source = source
             @target = target
+            @multival = multival
+            @delim = delim
           end
 
           # @private
           def process(row)
+            row[@target] = nil
             val = row.fetch(@source, nil)
-            if val.blank?
-              row[@target] = nil
-            else
-              val = val.unicode_normalized?(:nfkc) ? val : val.unicode_normalize(:nfkc)
-              BRUTEFORCE.each { |k, v| val = val.gsub(k, v) }
-              norm = ActiveSupport::Inflector.transliterate(val)
-              norm = norm.gsub(/\W/, '')
-              row[@target] = norm.downcase
-            end
+            return row if val.blank?
+
+            row[@target] = values(val).map{ |val| normalize(val) }.join(@delim)
             row
+          end
+
+          private
+
+          def normalize(val)
+            val = val.unicode_normalized?(:nfkc) ? val : val.unicode_normalize(:nfkc)
+            BRUTEFORCE.each { |k, v| val = val.gsub(k, v) }
+            ActiveSupport::Inflector.transliterate(val).gsub(/\W/, '').downcase
+          end
+          
+          def values(val)
+            return [val] unless @multival
+
+            val.split(@delim)
           end
         end
       end
