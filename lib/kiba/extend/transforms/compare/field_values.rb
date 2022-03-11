f1ea0dfcfeb5ea14863a7668ce64c08e5a90efbe
Kristina Spurgin <kristina.spurgin@lyrasis.org>
2022-03-11 10:55:15 -0500

    add inline docs

--- lib/kiba/extend/transforms/compare/field_values.rb
+++ lib/kiba/extend/transforms/compare/field_values.rb
@@ -4,126 +4,83 @@ module Kiba
   module Extend
     module Transforms
       module Compare
-        # Adds the given field(s) to the row with nil value if they do not already exist in row
+        # @since 2.7.1.62
+        # Compares values in the given fields and puts `same` or `diff` in the given target field.
         #
         # # Examples
         #
-        # Input table is not shown separately. It is just `name` column of the results tables shown below. The blank
-        #   value in name indicates an empty Ruby String object. The nil indicates a Ruby NilValue object.
+        # Input table:
         #
-        # ## Example 1
-        # No placeholder value is given, so "NULL" is treated as a string value. `count_empty` defaults to false, so
-        #   empty values are not counted.
+        # ```
+        # | id  | pid | zid |
+        # |-----+-----+-----|
+        # | a   | a   | a   |
+        # | A   | a   | a   |
+        # | a   |  a  |  a  |
+        # |     | a   | a   |
+        # | nil | a   | a   |
+        # |     | nil |     |
+        # ```
+        #
+        # The values in the third row are id = 'a ', pid = ' a', zid = ' a '.
         #
         # Used in pipeline as:
         #
         # ```
-        #  transform Compare::FieldValues, field: :name, target: :ct, delim: ';'
+        #  transform Compare::FieldValues, fields: %i[id pid zid], target: :comp
         # ```
         #
         # Results in:
         #
         # ```
-        # | name                 | ct |
-        # |----------------------+----|
-        # | Weddy                | 1  |
-        # | NULL                 | 1  |
-        # |                      | 0  |
-        # | nil                  | 0  |
-        # | Earlybird;Divebomber | 2  |
-        # | ;Niblet              | 1  |
-        # | Hunter;              | 1  |
-        # | NULL;Earhart         | 2  |
-        # | ;                    | 0  |
-        # | NULL;NULL            | 2  |
+        # | id  | pid | zid | comp |
+        # |-----+-----+-----+------|
+        # | a   | a   | a   | same |
+        # | A   | a   | a   | same |
+        # | a   |  a  |  a  | same |
+        # |     | a   | a   | diff |
+        # | nil | a   | a   | diff |
+        # |     | nil |     | same |
         # ```
-        #
-        # ## Example 2
-        # No placeholder value is given, so "NULL" is treated as a string value. `count_empty` is true, so
-        #   empty values are counted. Note that fully empty field values are not counted, but when a
-        #   delimiter is present, any empty values delimited by it are counted.
-        #
-        # These options are functionally equivalent to those shown in example 4.
         #
         # Used in pipeline as:
         #
         # ```
-        #  transform Compare::FieldValues, field: :name, target: :ct, delim: ';', count_empty: true
+        #  transform Compare::FieldValues, fields: %i[id pid zid], target: :comp, downcase: false, strip: false
         # ```
         #
         # Results in:
         #
         # ```
-        # | name                 | ct |
-        # |----------------------+----|
-        # | Weddy                | 1  |
-        # | NULL                 | 1  |
-        # |                      | 0  |
-        # | nil                  | 0  |
-        # | Earlybird;Divebomber | 2  |
-        # | ;Niblet              | 2  |
-        # | Hunter;              | 2  |
-        # | NULL;Earhart         | 2  |
-        # | ;                    | 2  |
-        # | NULL;NULL            | 2  |
+        # | id  | pid | zid | comp |
+        # |-----+-----+-----+------|
+        # | a   | a   | a   | same |
+        # | A   | a   | a   | diff |
+        # | a   |  a  |  a  | diff |
+        # |     | a   | a   | diff |
+        # | nil | a   | a   | diff |
+        # |     | nil |     | same |
         # ```
-        #
-        # ## Example 3
-        # Placeholder value is given, so "NULL" is treated as an empty value. `count_empty` default to false, so empty
-        #   values, including any values that equal the given placeholder value, are not counted.
         #
         # Used in pipeline as:
         #
         # ```
-        #  transform Compare::FieldValues, field: :name, target: :ct, delim: ';', placeholder: 'NULL'
+        #  transform Compare::FieldValues, fields: %i[id pid zid], target: :comp, ignore_blank: true
         # ```
         #
         # Results in:
         #
         # ```
-        # | name                 | ct |
-        # |----------------------+----|
-        # | Weddy                | 1  |
-        # | NULL                 | 0  |
-        # |                      | 0  |
-        # | nil                  | 0  |
-        # | Earlybird;Divebomber | 2  |
-        # | ;Niblet              | 1  |
-        # | Hunter;              | 1  |
-        # | NULL;Earhart         | 1  |
-        # | ;                    | 0  |
-        # | NULL;NULL            | 0  |
+        # | id  | pid | zid | comp |
+        # |-----+-----+-----+------|
+        # | a   | a   | a   | same |
+        # | A   | a   | a   | same |
+        # | a   |  a  |  a  | same |
+        # |     | a   | a   | same |
+        # | nil | a   | a   | same |
+        # |     | nil |     | same |
         # ```
         #
-        # ## Example 4
-        # Placeholder value is given, so "NULL" is treated as an empty value. `count_empty` is true, so
-        #   empty values are counted. Note that fully empty field values are not counted, but when a
-        #   delimiter is present, any empty values delimited by it are counted.
-        #
-        # These options are functionally equivalent to those shown in example 2.
-        #
-        # Used in pipeline as:
-        #
-        # ```
-        #  transform Compare::FieldValues, field: :name, target: :ct, delim: ';', placeholder: 'NULL', count_empty: true
-        # ```
-        #
-        # Results in:
-        #
-        # ```
-        # | name                 | ct |
-        # |----------------------+----|
-        # | Weddy                | 1  |
-        # | NULL                 | 1  |
-        # |                      | 0  |
-        # | nil                  | 0  |
-        # | Earlybird;Divebomber | 2  |
-        # | ;Niblet              | 2  |
-        # | Hunter;              | 2  |
-        # | NULL;Earhart         | 2  |
-        # | ;                    | 2  |
-        # | NULL;NULL            | 2  |
-        # ```
         class FieldValues
           # @param fields [Array<Symbol>] names of fields whose values will be compared
           # @param target [Symbol] new field in which to record comparison result
