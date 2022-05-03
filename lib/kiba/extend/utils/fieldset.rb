7ad596ba4723ed035beb1953bde9364acaec8af8
Kristina Spurgin <kristina.spurgin@lyrasis.org>
2022-05-03 19:30:37 -0400

    add `null_placeholder` param to Fieldset

--- lib/kiba/extend/utils/fieldset.rb
+++ lib/kiba/extend/utils/fieldset.rb
@@ -5,24 +5,28 @@ module Kiba
     module Utils
       # Data structure class used in processing merge transforms
       class Fieldset
+        attr_reader :hash
+        
         def initialize(fields:, null_placeholder: nil)
+          @null_placeholder = null_placeholder
           @hash = {}
-          fields.each { |field| @hash[field] = [] }
+          fields.each { |field| hash[field] = [] }
         end
 
         def add_constant_values(field, value)
-          @hash[field] = []
-          value_ct.times { @hash[field] << value }
+          hash[field] = []
+          value_ct.times { hash[field] << value }
         end
 
         def fields
-          @hash.keys
+          hash.keys
         end
 
-        attr_reader :hash
-
         def join_values(delim)
-          @hash.transform_values! { |vals| vals.join(delim) }
+          hash.transform_values! do |vals|
+            placeholdered = null_placeholder ? vals.map{ |val| val.nil? ? null_placeholder : val } : vals
+            placeholdered.join(delim)
+          end
         end
 
         def populate(rows)
@@ -33,28 +37,30 @@ module Kiba
         end
 
         def value_ct
-          @hash.values.first.length
+          hash.values.first.length
         end
 
         private
+
+        attr_reader :null_placeholder
 
         def get_field_values(row)
           fields.each do |field|
             fetched = row.fetch(field, nil)
             value = fetched.blank? ? nil : fetched
-            @hash[field] << value
+            hash[field] << value
           end
         end
 
         def remove_valueless_rows
           valueless_indices.each do |index|
-            @hash.each { |_field, values| values.delete_at(index) }
+            hash.each { |_field, values| values.delete_at(index) }
           end
         end
 
         def valueless_indices
           indices = []
-          @hash.values.first.each_with_index do |_element, i|
+          hash.values.first.each_with_index do |_element, i|
             indices << i if @hash.values.map { |vals| vals[i] }.compact.empty?
           end
           indices.sort.reverse
