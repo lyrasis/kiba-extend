b160e7306c9368cb786883aebf3211dd0b690978
Kristina Spurgin <kristina.spurgin@lyrasis.org>
2022-05-06 21:44:10 -0400

    warn if `to` field exists and will be overwritten; add docs

--- lib/kiba/extend/transforms/rename/field.rb
+++ lib/kiba/extend/transforms/rename/field.rb
@@ -4,9 +4,33 @@ module Kiba
   module Extend
     module Transforms
       module Rename
+        # Renames one field
+        # # Examples
+        #
+        # Input rows:
+        #
+        # ```
+        # {name: 'Weddy', sex: 'm'},
+        # {name: 'Kernel', sex: 'f'}
+        # ```
+        #
+        # Used in pipeline as:
+        #
+        # ```
+        # transform Rename::Field, from: :sex, to: :gender
+        # ```
+        #
+        # Results in:
+        #
+        # ```
+        # {name: 'Weddy', gender: 'm'},
+        # {name: 'Kernel', gender: 'f'}
+        # ```
         class Field
           include SingleWarnable
 
+          # @param from Symbol current field name
+          # @param to Symbol target field name
           def initialize(from:, to:)
             @from = from
             @to = to
@@ -19,6 +43,8 @@ module Kiba
               add_single_warning("Cannot rename field: `#{from}` does not exist in row")
               return row
             end
+
+            add_single_warning("Renaming `#{from}` to `#{to}` overwrites existing `#{to}` field data") if row.key?(to)
             
             row[to] = row.fetch(from)
             row.delete(from)
