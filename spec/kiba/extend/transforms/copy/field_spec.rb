338fc0b233a3cf01d39665208ad5ba17738908bb
Kristina Spurgin <kristina.spurgin@lyrasis.org>
2022-05-09 18:11:06 -0400

    `Copy::Field` raises error if `from` field is missing

---
+++ spec/kiba/extend/transforms/copy/field_spec.rb
@@ -0,0 +1,41 @@
+# frozen_string_literal: true
+
+require 'spec_helper'
+
+RSpec.describe Kiba::Extend::Transforms::Copy::Field do
+  let(:input) do
+    [
+    {name: 'Weddy', sex: 'm'},
+    {name: 'Kernel', sex: 'f'},
+    {name: 'keet', sex: ''},
+    {name: 'keet', sex: nil}
+    ]
+  end
+  
+  let(:transform){ described_class.new(from: :sex, to: :gender) }
+
+  let(:expected) do
+    [
+      { name: 'Weddy', sex: 'm', gender: 'm' },
+      { name: 'Kernel', sex: 'f', gender: 'f' },
+      {name: 'keet', sex: '', gender: ''},
+      {name: 'keet', sex: nil, gender: nil}
+    ]
+  end
+
+  let(:result){ input.map{ |row| transform.process(row) } }
+    
+  it 'copies value of field to specified new field' do
+    expect(result).to eq(expected)
+  end
+
+  context 'when from field does not exist' do
+    let(:transform){ described_class.new(from: :foo, to: :gender) }
+
+    it 'copies value of field to specified new field' do
+      msg = 'Cannot copy from nonexistent field `foo`'
+      expect{ result }.to raise_error(Kiba::Extend::Transforms::Copy::Field::MissingFromFieldError).with_message(msg)
+    end
+  end
+end
+
