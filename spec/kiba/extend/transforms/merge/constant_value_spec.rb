5fb04c2096f54ac2601afaadb9a96ba82a399507
Kristina Spurgin <kristina.spurgin@lyrasis.org>
2022-05-16 18:07:01 -0400

    test that present but unpopulated target field does not send warning

--- spec/kiba/extend/transforms/merge/constant_value_spec.rb
+++ spec/kiba/extend/transforms/merge/constant_value_spec.rb
@@ -5,12 +5,6 @@ require 'spec_helper'
 RSpec.describe Kiba::Extend::Transforms::Merge::ConstantValue do
   let(:transform){ described_class.new(target: :species, value: 'guinea fowl') }
   
-  let(:rows) do
-    [
-      {name: 'Weddy', sex: 'm', source: 'adopted'},
-      {name: 'Kernel', sex: 'f', source: 'adopted', species: 'Numida meleagris'}
-    ]
-  end
   let(:results){ rows.map{ |row| transform.process(row) } }
   let(:expected) do
     [
@@ -19,9 +13,32 @@ RSpec.describe Kiba::Extend::Transforms::Merge::ConstantValue do
     ]
   end
 
-  it 'transforms and warns as expected', :aggregate_failures do
-    msg = "#{Kiba::Extend.warning_label}: Any values in existing `species` field will be overwritten with `guinea fowl`"
-    expect(transform).to receive(:warn).with(msg)
-    expect(results).to eq(expected)
+  context 'target field exists and is populated' do
+    let(:rows) do
+      [
+        {name: 'Weddy', sex: 'm', source: 'adopted'},
+        {name: 'Kernel', sex: 'f', source: 'adopted', species: 'Numida meleagris'}
+      ]
+    end
+
+    it 'transforms and warns as expected', :aggregate_failures do
+      msg = "#{Kiba::Extend.warning_label}: Any values in existing `species` field will be overwritten with `guinea fowl`"
+      expect(transform).to receive(:warn).with(msg)
+      expect(results).to eq(expected)
+    end
+  end
+
+  context 'target field exists and is not populated' do
+    let(:rows) do
+      [
+        {name: 'Weddy', sex: 'm', source: 'adopted'},
+        {name: 'Kernel', sex: 'f', source: 'adopted', species: ''}
+      ]
+    end
+
+    it 'transforms and does not warn as expected', :aggregate_failures do
+      expect(transform).not_to receive(:warn)
+      expect(results).to eq(expected)
+    end
   end
 end
