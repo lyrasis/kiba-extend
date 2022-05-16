9571481bcc88c037346b4c0196e2ec2daf43a58e
Kristina Spurgin <kristina.spurgin@lyrasis.org>
2022-05-16 17:43:23 -0400

    Merge::ConstantValue warns if target field exists

--- lib/kiba/extend/transforms/merge/constant_value.rb
+++ lib/kiba/extend/transforms/merge/constant_value.rb
@@ -4,17 +4,31 @@ module Kiba
   module Extend
     module Transforms
       module Merge
+        # Merges given value into the given target field in every row. Target field is added new. If it already exists,
+        #   values in target field are overridden by contant value. 
         class ConstantValue
+          include SingleWarnable
+          # @param target [Symbol] target field in which to enter constant value
+          # @param value [String] the constant value to enter in target field 
           def initialize(target:, value:)
             @target = target
             @value = value
+            setup_single_warning
           end
 
           # @private
           def process(row)
-            row[@target] = @value
+            unless row.fetch(target, nil).blank?
+              add_single_warning("Any values in existing `#{target}` field will be overwritten with `#{value}`")
+            end
+            
+            row[target] = value
             row
           end
+
+          private
+
+          attr_reader :target, :value
         end
       end
     end
--- spec/kiba/extend/transforms/merge/constant_value_spec.rb
+++ spec/kiba/extend/transforms/merge/constant_value_spec.rb
@@ -3,23 +3,15 @@
 require 'spec_helper'
 
 RSpec.describe Kiba::Extend::Transforms::Merge::ConstantValue do
-  let(:accumulator){ [] }
-  let(:test_job){ Helpers::TestJob.new(input: input, accumulator: accumulator, transforms: transforms) }
-  let(:result){ test_job.accumulator }
-
-  let(:input) do
+  let(:transform){ described_class.new(target: :species, value: 'guinea fowl') }
+  
+  let(:rows) do
     [
       {name: 'Weddy', sex: 'm', source: 'adopted'},
-      {name: 'Kernel', sex: 'f', source: 'adopted'}
+      {name: 'Kernel', sex: 'f', source: 'adopted', species: 'Numida meleagris'}
     ]
   end
-
-  let(:transforms) do
-    Kiba.job_segment do
-      transform Merge::ConstantValue, target: :species, value: 'guinea fowl'
-    end
-  end
-
+  let(:results){ rows.map{ |row| transform.process(row) } }
   let(:expected) do
     [
       {name: 'Weddy', sex: 'm', source: 'adopted', species: 'guinea fowl' },
@@ -27,7 +19,9 @@ RSpec.describe Kiba::Extend::Transforms::Merge::ConstantValue do
     ]
   end
 
-  it 'merges specified constant data values into row' do
-    expect(result).to eq(expected)
+  it 'transforms and warns as expected', :aggregate_failures do
+    msg = "#{Kiba::Extend.warning_label}: Any values in existing `species` field will be overwritten with `guinea fowl`"
+    expect(transform).to receive(:warn).with(msg)
+    expect(results).to eq(expected)
   end
 end
