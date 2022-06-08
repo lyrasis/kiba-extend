ebb89bc4aa0b645dd6890f9f3699bdcb0873e056
Kristina Spurgin <kristina.spurgin@lyrasis.org>
2022-06-08 13:02:39 -0400

    remove Merge::ConstantValueConditional example from RowSelectorByLambda yarddoc

--- lib/kiba/extend/utils/lookup/row_selector_by_lambda.rb
+++ lib/kiba/extend/utils/lookup/row_selector_by_lambda.rb
@@ -5,15 +5,17 @@ module Kiba
     module Utils
       module Lookup
         # Called by transforms that have a `conditions` parameter, when a Lambda is passed in as the
-        #   conditions. This is a more straightforward and flexible method of controlling which rows
+        #   conditional logic. This is a more straightforward and flexible method of controlling which rows
         #   are selected/operated on than the hideous Hash conditions format.
         #
-        # The Lambda passed in must be defined with two arguments. The first should be a single
+        # The lambda Proc passed in must be defined with two arguments. The first should be a single
         #   data row, typically the source/target row for a data merge or other operation. The
         #   second is an array of data rows.
         #
         # Because of how this RowSelector is called by different transforms, both arguments need to be
         #   defined, even if you aren't going to use them in the logic encapsulated in the Lambda.
+        #
+        # The lambda Proc must return an array of rows meeting the given criteria.
         #
         # ## Examples
         #
@@ -55,18 +57,6 @@ module Kiba
         #   In addition, the addresses table indicates whether each address is active. This will cause
         #   no addresses to be merged in for inactive contacts. For active contacts, only active
         #   addresses will be merged in.
-        #
-        # In your job transforms:
-        #
-        # ```
-        # condition = ->(row, _x){ row.select{ |r| r[:note].match?(/gift|donation/i) } }
-        # transform Merge::ConstantValueConditional, 
-        #   fieldmap: { reason: 'gift' },
-        #   conditions: condition
-        # ```
-        #
-        # This will merge the constant value `gift` into the `reason` field if the `note` field includes `gift`
-        #   or `donation` (case insensitive).
         #
         # @since 2.8.0
         class RowSelectorByLambda
