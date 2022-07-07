b13cc3ee32f375d25e1c03c25f2c411c279bf88c
Kristina Spurgin <kristina.spurgin@lyrasis.org>
2022-07-07 15:48:06 -0400

    add related transforms to yarddoc

--- lib/kiba/extend/transforms/cspace.rb
+++ lib/kiba/extend/transforms/cspace.rb
@@ -4,6 +4,20 @@ module Kiba
   module Extend
     module Transforms
       # Transformations specific to preparing data for import into CollectionSpace
+      #
+      # ## Other transforms of special relevance to CollectionSpace jobs
+      #
+      # - {{Append::ConvertedValueAndUnit}} - For augmenting Object dimension values
+      # - {{Name}} transforms for Person authorities
+      # - {{Fingerprint}} transforms for preparing cleanup worksheets for clients and merging completed worksheets
+      #   back into migration projects
+      # - {{Reshape::CollapseMultipleFieldsToOneTypedFieldPair}} - often useful for preparing phone type and email type fields
+      # - {{CombineValues::AcrossFieldGroup}} and {{Cspace::FieldGroupCombiner}} - different approaches to generating data for
+      #   repeatable field groups. {{CombineValues::AcrossFieldGroup}} makes sense if the data to be combined exists in basically
+      #   the right "shape" for combining. You can just define what source fields map to what target fields without renaming
+      #   fields in a particular way. When you have to explode/reshape/create source data into a combinable form, and you can
+      #   do that buy creating fields using a consistent naming convention, {{Cspace::FieldGroupCombiner}} is a simpler transform
+      #   to set up, and applies {{Clean::EmptyFieldGroups}} automatically
       module Cspace
         ::Cspace = Kiba::Extend::Transforms::Cspace
         # Characters or character combinations known to be treated strangely by CollectionSpace when creating IDs. Used as a lookup to force the substitution we need
