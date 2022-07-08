f9fa478e7aab213de58d189bdc2b8ad727b3eddb
Kristina Spurgin <kristina.spurgin@lyrasis.org>
2022-07-08 19:19:16 -0400

    document related transform namespaces

--- lib/kiba/extend/transforms/collapse.rb
+++ lib/kiba/extend/transforms/collapse.rb
@@ -4,6 +4,14 @@ module Kiba
   module Extend
     module Transforms
       # Transforms that turn fields or values into a smaller number of fields or values
+      #
+      # ## See also
+      #
+      # - {{Kiba::Extend::Transforms::Take}} transforms, which select a single value from multivalue
+      #   fields
+      # - {{Kiba::Extend::Transforms::FilterRows}} transforms, which can be used to conditionally
+      #   remove rows from a job
+      # - {{Kiba::Extend::Transforms::Extract}} transforms, which can produce subsets of data
       module Collapse
         ::Collapse = Kiba::Extend::Transforms::Collapse
       end
