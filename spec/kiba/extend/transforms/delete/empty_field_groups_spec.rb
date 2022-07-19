fa21514c500a7255cc0025f017d2f218698e7760
Kristina Spurgin <kristina.spurgin@lyrasis.org>
2022-07-18 21:08:34 -0400

    add test

--- spec/kiba/extend/transforms/delete/empty_field_groups_spec.rb
+++ spec/kiba/extend/transforms/delete/empty_field_groups_spec.rb
@@ -18,6 +18,7 @@ RSpec.describe Kiba::Extend::Transforms::Delete::EmptyFieldGroups do
       {aa: '|', ab: '|', bb: 'e||n|', bc: 'n||e|', bd: 'e||p|'},
       {aa: '%NULLVALUE%', ab: '%NULLVALUE%', bb: '%NULLVALUE%|%NULLVALUE%', bc: nil, bd: '|'},
       {aa: '|', ab: '', bb: '%NULLVALUE%|', bc: '%NULLVALUE%|%NULLVALUE%', bd: '%NULLVALUE%|a'},
+      {aa: '|', ab: '', bb: '%NULLVALUE%|', bc: '%NULLVALUE%|NULL', bd: '%NULLVALUE%|a'}
     ]
   end
 
@@ -32,6 +33,28 @@ RSpec.describe Kiba::Extend::Transforms::Delete::EmptyFieldGroups do
         {aa: nil, ab: nil, bb: 'e|n', bc: 'n|e', bd: 'e|p'},
         {aa: nil, ab: nil, bb: nil, bc: nil, bd: nil},
         {aa: nil, ab: nil, bb: nil, bc: nil, bd: 'a'},
+        {aa: nil, ab: nil, bb: nil, bc: 'NULL', bd: 'a'}
+      ]
+    end
+
+    it 'deletes as expected' do
+      expect(result).to eq(expected)
+    end
+  end
+
+  context 'with array of treat_as_empty values: [%NULLVALUE%, NULL]' do
+    let(:params){ {groups: groups, delim: delim, treat_as_null: ['NULL', '%NULLVALUE%']} }
+    let(:expected) do
+      [
+        {aa: nil, ab: nil, bb: nil, bc: nil, bd: nil},
+        {aa: 'n', ab: nil, bb: 'e', bc: 'n', bd: 'e'},
+        {aa: 'n', ab: nil, bb: 'n', bc: 'e', bd: 'p'},
+        {aa: 'n|e', ab: 'e|n', bb: 'n|e', bc: 'e|n', bd: 'ne|'},
+        {aa: 'n', ab: 'e', bb: '|e', bc: 'n|e', bd: '|e'},
+        {aa: nil, ab: nil, bb: 'e|n', bc: 'n|e', bd: 'e|p'},
+        {aa: nil, ab: nil, bb: nil, bc: nil, bd: nil},
+        {aa: nil, ab: nil, bb: nil, bc: nil, bd: 'a'},
+        {aa: nil, ab: nil, bb: nil, bc: nil, bd: 'a'}
       ]
     end
 
@@ -52,6 +75,7 @@ RSpec.describe Kiba::Extend::Transforms::Delete::EmptyFieldGroups do
         {aa: nil, ab: nil, bb: 'e|n', bc: 'n|e', bd: 'e|p'},
         {aa: '%NULLVALUE%', ab: '%NULLVALUE%', bb: '%NULLVALUE%|%NULLVALUE%', bc: nil, bd: nil},
         {aa: nil, ab: nil, bb: '%NULLVALUE%|', bc: '%NULLVALUE%|%NULLVALUE%', bd: '%NULLVALUE%|a'},
+        {aa: nil, ab: nil, bb: '%NULLVALUE%|', bc: '%NULLVALUE%|NULL', bd: '%NULLVALUE%|a'}
       ]
     end
 
