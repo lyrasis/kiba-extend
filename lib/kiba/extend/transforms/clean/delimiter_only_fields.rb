379f727825a6d89b1c2e61985be45fa72353a0ee
Kristina Spurgin <kristina.spurgin@lyrasis.org>
2022-07-18 21:22:35 -0400

    finish up deprecated transform warnings and changelog future removal notes

--- lib/kiba/extend/transforms/clean/delimiter_only_fields.rb
+++ lib/kiba/extend/transforms/clean/delimiter_only_fields.rb
@@ -7,12 +7,15 @@ module Kiba
         # @deprecated in 2.9.0. Use {Delete::DelimiterOnlyFieldValues} instead
         class DelimiterOnlyFields
           def initialize(delim:, use_nullvalue: false)
+            
             nullval = use_nullvalue ? Kiba::Extend.nullvalue : nil
             @replacement = Delete::DelimiterOnlyFieldValues.new(
               fields: :all,
               delim: delim,
               treat_as_null: nullval
             )
+            msg = 'Clean::DelimiterOnlyFields to be deprecated in a future release. Convert any usage of this transform to Delete::DelimiterOnlyFieldValues'
+            warn("#{Kiba::Extend.warning_label}: #{msg}")
           end
 
           # @private
