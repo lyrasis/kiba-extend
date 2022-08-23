721dc5b5359bca1f3d3992f842a37d806d48f97d
Jacob Shelby <jacobzaccount@gmail.com>
2022-08-23 14:59:03 -0400

    added test for latest fix to Split:IntoMultipleColumns

--- spec/kiba/extend/transforms/split_spec.rb
+++ spec/kiba/extend/transforms/split_spec.rb
@@ -104,5 +104,25 @@ RSpec.describe Kiba::Extend::Transforms::Split do
         expect(results).to eq(expected)
       end
     end
+
+    context 'when longest split value (4) > max segments (2) and collapses_on :right' do
+      let(:rows) do
+        [
+          {name: 'Barrow, David Brown, Jr., 1931-'},
+          {name: 'Beaman, Warren M., II, 1921-2016'}
+        ]
+      end
+
+      let(:params){ {field: :name, sep: ',', max_segments: 2, collapse_on: :right} }
+
+      it 'collapses on right' do
+        expected = [
+          { name0: 'Barrow', name1: 'David Brown,Jr.,1931-' },
+          { name0: 'Beaman', name1: 'Warren M.,II,1921-2016' }
+        ]
+        expect(results).to eq(expected)
+      end
+    end
+
   end
 end
