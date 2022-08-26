2b2614bfa5389b92bb03256ae5a5cdf1e58b515c
Kristina Spurgin <kristina.spurgin@lyrasis.org>
2022-08-26 10:33:03 -0400

    delete deprecated transforms

--- lib/kiba/extend/transforms/filter_rows.rb
+++ lib/kiba/extend/transforms/filter_rows.rb
@@ -6,28 +6,6 @@ module Kiba
       # Transformations that remove rows based on different types of conditions
       module FilterRows
         ::FilterRows = Kiba::Extend::Transforms::FilterRows
-
-        # @deprecated Convert any uses of this transform in your jobs to
-        #   {Kiba::Extend::Transforms::FilterRows::WithLambda}
-        class FieldValueGreaterThan
-          def initialize(action:, field:, value:)
-            warn("#{self.class.name} will be removed in a future version. Convert to `FilterRows::WithLambda`", category: :deprecated)
-            @action = action
-            @field = field
-            @value = value
-          end
-
-          # @param row [Hash{ Symbol => String }]
-          def process(row)
-            val = row.fetch(@field)
-            case @action
-            when :keep
-              val > @value ? row : nil
-            when :reject
-              val > @value ? nil : row
-            end
-          end
-        end
       end
     end
   end
