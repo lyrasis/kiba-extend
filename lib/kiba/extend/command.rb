58116035a8be1fd865f278b56f32cd0a6485e049
Kristina Spurgin <kristina.spurgin@lyrasis.org>
2022-09-09 19:51:24 -0400

    remove copied comment

--- lib/kiba/extend/command.rb
+++ lib/kiba/extend/command.rb
@@ -3,8 +3,6 @@
 module Kiba
   module Extend
     # Modules and classes defining commands
-    #
-    # This structure follows the pattern set out in {https://github.com/thbar/kiba-common kiba-common}
     module Command
     end
   end
