4a42e3f322dcc22c7e58ae5e5a8300d90ea9cb56
kspurgin <kspurgin@users.noreply.github.com>
2022-09-22 18:08:30 -0400

    `Fraction::ToDecimal` transform (#108)
    * add Data::ConvertibleFraction value object class
    * add Utils::ExtractFractions service object
    * add Fraction::ToDecimal transform

---
+++ lib/kiba/extend/utils/extract_fractions.rb
@@ -0,0 +1,78 @@
+# frozen_string_literal: true
+
+require 'strscan'
+
+module Kiba
+  module Extend
+    module Utils
+      # Extracts {Data::ConvertibleFractions} from given String and returns only fractions that can be
+      #   converted to decimal, in the order they will need to be replaced in the string
+      class ExtractFractions
+        # @param whole_fraction_sep [Array(String)] List of characters that precede a fraction after a whole
+        #   number, indicating that the whole number and fraction should be extracted together. If this is
+        #   set to `[' ', '-']` (the default), then both `1 1/2` and `1-1/2` will be extracted with `1` as
+        #   the whole number and `1/2` as the fraction, and converted to `1.5`. If this is set to `[' ']`,
+        #   then `1 1/2` will be extracted as described preveiously. For `1-1/2`, no whole number value
+        #   will be extracted. `1/2` will be extracted as the fraction, and it will be converted to '0.5'.
+        def initialize(whole_fraction_sep: [' ', '-'])
+          @whole_fraction_sep = whole_fraction_sep
+          @fpattern = /(\d+\/\d+)/
+          @fraction = Kiba::Extend::Data::ConvertibleFraction
+        end
+
+        # @param value [String]
+        def call(value)
+          return [] unless value.match?(fpattern)
+          
+          result = []
+          scanner = StringScanner.new(value)
+          scan(scanner, result)
+          result.each do |fraction|
+            unless fraction.convertible?
+              warn("#{self.class.name}: Unconvertible fraction: #{value[fraction.position]}")
+            end
+          end
+          result.sort.reverse
+        end
+        
+        private
+
+        attr_reader :fpattern, :whole_fraction_sep, :fraction
+
+        def extract_fraction(scanner, result)
+          startpos = scanner.pos
+          scanner.scan(fpattern)
+          result << fraction.new(**{fraction: scanner.captures[0], position: startpos..scanner.pos - 1 })
+        end
+
+        def try_whole_fraction_extract(scanner, result)
+          startpos = scanner.pos
+          whole_num = scanner.scan(/\d+/).to_i
+          sep = scanner.scan(/./)
+          fmatch = scanner.match?(fpattern)
+          if whole_fraction_sep.any?(sep) && fmatch
+            result << fraction.new(**{whole: whole_num, fraction: scanner.scan(fpattern), position: startpos..scanner.pos - 1 })
+          end
+        end
+
+        def scan(scanner, result)
+          return if scanner.eos?
+          return if scanner.rest_size < 3
+          return unless scanner.exist?(fpattern)
+
+          scan_next(scanner, result)
+        end
+
+        def scan_next(scanner, result)
+          scanner.skip(/\D+/)
+          if scanner.match?(fpattern)
+            extract_fraction(scanner, result)
+          else
+            try_whole_fraction_extract(scanner, result)
+          end
+          scan(scanner, result)
+        end
+      end
+    end
+  end
+end
---
+++ spec/kiba/extend/utils/extract_fractions_spec.rb
@@ -0,0 +1,98 @@
+# frozen_string_literal: true
+
+require 'spec_helper'
+
+def fraction(params)
+  Kiba::Extend::Data::ConvertibleFraction.new(**params)
+end
+
+RSpec.describe Kiba::Extend::Utils::ExtractFractions do
+  subject(:xform){ described_class.new(**params) }
+
+  # describe '.initialize' do
+  #   context 'with defaults' do
+  #     let(:params){ {} }
+
+  #     it 'has expected instance variables' do
+  #       expect
+  #     end
+  #   end
+  # end
+  
+  describe '#call' do
+    let(:result){ xform.call(val) }
+    let(:results){ expectations.keys.map{ |val| xform.call(val) } }
+    let(:expected){ expectations.values }
+
+    context %{with defaults} do
+      let(:params){ {} }
+      let(:expectations) do
+        {
+          '2/3 x 9-7/8 x 1/4, 20, 3 3/4' => [
+            fraction({whole: 3, fraction: '3/4', position: 23..27}),
+            fraction({fraction: '1/4', position: 14..16}),
+            fraction({whole: 9, fraction: '7/8', position: 6..10}),
+            fraction({fraction: '2/3', position: 0..2})
+          ],
+          '6-1/2 x 9-1/4 and height unknown' => [
+            fraction({whole: 9, fraction: '1/4', position: 8..12}),
+            fraction({whole: 6, fraction: '1/2', position: 0..4})
+          ],
+          '123' => [],
+          'measures 1/4ft' => [
+            fraction({fraction: '1/4', position: 9..11})
+          ],
+          '7/16-1/4' => [
+            fraction({fraction: '1/4', position: 5..7}),
+            fraction({fraction: '7/16', position: 0..3})
+          ]
+        }
+      end
+
+      it 'returns expected' do
+        expect(results).to eq(expected)
+      end
+    end
+
+    context %{with only ' ' as whole_fraction_sep} do
+      let(:params){ {whole_fraction_sep: [' ']} }
+      let(:expectations) do
+        {
+          '2/3 x 9-7/8 x 1/4, 20, 3 3/4' => [
+            fraction({whole: 3, fraction: '3/4', position: 23..27}),
+            fraction({fraction: '1/4', position: 14..16}),
+            fraction({fraction: '7/8', position: 8..10}),
+            fraction({fraction: '2/3', position: 0..2})
+          ],
+          '6-2/3 x 9-1/4 and height unknown' => [
+            fraction({fraction: '1/4', position: 10..12}),
+            fraction({fraction: '2/3', position: 2..4}),
+          ],
+          '123' => [],
+          'measures 1/4ft' => [
+            fraction({fraction: '1/4', position: 9..11})
+          ],
+          '7/16-1/4' => [
+            fraction({fraction: '1/4', position: 5..7}),
+            fraction({fraction: '7/16', position: 0..3})
+          ]
+        }
+      end
+
+      it 'returns expected' do
+        expect(results).to eq(expected)
+      end
+    end
+
+    context %{with un-convertable "fraction"} do
+      let(:params){ {} }
+      let(:value){ 'copy 1/0' }
+      let(:result){ xform.call(value) }
+      it 'returns expected' do
+        msg = 'Kiba::Extend::Utils::ExtractFractions: Unconvertible fraction: 1/0'
+        expect(xform).to receive(:warn).with(msg)
+        expect(result).to eq([fraction({fraction: '1/0', position: 5..7})])
+      end
+    end
+  end
+end
