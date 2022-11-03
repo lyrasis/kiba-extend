540c1159bd0b97a714d5c3926879243436447fb6
Kristina Spurgin <kristina.spurgin@lyrasis.org>
2022-11-03 12:00:20 -0400

    add additional tests

--- spec/kiba/extend/transforms/deduplicate/grouped_field_values_spec.rb
+++ spec/kiba/extend/transforms/deduplicate/grouped_field_values_spec.rb
@@ -12,20 +12,56 @@ RSpec.describe Kiba::Extend::Transforms::Deduplicate::GroupedFieldValues do
 
   let(:input) do
     [
-        {name: 'Fred;Freda;Fred;James', work: 'Report;Book;Paper;Book', role: 'author;photographer;editor;illustrator'},
-        {name: ';', work: ';', role: ';'},
-        {name: 'Martha', work: 'Book', role: 'contributor'}
+      {name: ';',
+       work: ';',
+       role: ';'},
+      {name: ';',
+       work: nil,
+       role: ';'},
+      {name: ';',
+       work: ';',
+       role: 'author;editor'},
+      {name: 'Bob;Jane;Bob',
+       work: ';',
+       role: 'auth;ed;contrib'},
+      {name: 'Bob;Jane;Bob',
+       work: '',
+       role: 'auth;ed'},
+      {name: 'Fred;Freda;Fred;James;Fred',
+       work: 'Report;Book;Paper;Book;Presentation',
+       role: 'author;photographer;editor;illustrator;author'},
+      {name: 'Martha',
+       work: 'Book',
+       role: 'contributor'}
     ]
   end
 
     let(:expected) do
       [
-        { name: 'Fred;Freda;James', work: 'Report;Book;Book', role: 'author;photographer;illustrator' },
-        { name: nil, work: nil, role: nil },
-        {name: 'Martha', work: 'Book', role: 'contributor'}
+        {name: nil,
+         work: nil,
+         role: nil},
+        {name: nil,
+         work: nil,
+         role: nil},
+        {name: nil,
+         work: nil,
+         role: nil},
+        {name: 'Bob;Jane',
+         work: nil,
+         role: 'auth;ed'},
+        {name: 'Bob;Jane',
+         work: nil,
+         role: 'auth;ed'},
+        {name: 'Fred;Freda;James',
+         work: 'Report;Book;Book',
+         role: 'author;photographer;illustrator'},
+        {name: 'Martha',
+         work: 'Book',
+         role: 'contributor'}
       ]
     end
-    
+
     it 'deduplicates indicates non-duplicates with `n`' do
       expect(result).to eq(expected)
     end
