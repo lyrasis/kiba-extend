6f83a2ef4ccad7decec5f551f20183aa71474357
Kristina Spurgin <kristina.spurgin@lyrasis.org>
2022-11-03 16:31:21 -0400

    convert Utils::SepDeprecator to Transforms::SepDeprecatable mixin module

---
+++ lib/kiba/extend/transforms/sep_deprecatable.rb
@@ -0,0 +1,58 @@
+# frozen_string_literal: true
+
+module Kiba
+  module Extend
+    module Transforms
+      # Mixin module to handle `sep` and `delim` params in Transforms classes
+      #   that are deprecating the `sep` param in favor of `delim`, but haven't
+      #   removed `sep` yet.
+      #
+      # ## Use/implementation
+      #
+      # Classes deprecating `sep` should:
+      #
+      # 1.
+      #
+      # ```
+      # include SepDeprecatable
+      # ```
+      #
+      # 2. Include in :initialize params:
+      #
+      # ```
+      # sep: nil, delim: nil
+      # ```
+      #
+      # 3. Include in class :initialize method:
+      #
+      # ```
+      # @delim = usedelim(sepval: sep, delimval: delim, calledby: self)
+      # ```
+      module SepDeprecatable
+        def usedelim(sepval:, delimval:, calledby:)
+          if delimval && !sepval
+            delimval
+          elsif !delimval && sepval
+            warn("#{Kiba::Extend.warning_label}:\n"\
+                 "  #{calledby.class}: `sep` parameter will be "\
+                 "deprecated in a future release.\n"\
+                 "TO FIX:\n"\
+                 "  Change `sep` to `delim`"
+                )
+            sepval
+          elsif delimval && sepval
+            warn("#{Kiba::Extend.warning_label}:\n"\
+                 "  #{calledby.class}: `sep` and `delim` parameters "\
+                 "given. `delim` value used. `sep` value ignored. "\
+                 "`sep` will be deprecated in a future release.\n"\
+                 "TO FIX:\n  Remove `sep` param"
+                )
+            delimval
+          else
+            fail(ArgumentError, "#{calledby.class}: missing keyword: :delim")
+          end
+        end
+      end
+    end
+  end
+end
---
+++ spec/kiba/extend/transforms/sep_deprecatable_spec.rb
@@ -0,0 +1,64 @@
+# frozen_string_literal: true
+
+require 'spec_helper'
+
+RSpec.describe Kiba::Extend::Transforms::SepDeprecatable do
+  class Xform
+    include Kiba::Extend::Transforms::SepDeprecatable
+  end
+
+  subject(:mod){ Xform.new }
+
+  describe '#usedelim' do
+    let(:result){ mod.usedelim(**params) }
+
+    context 'with delim and no sep' do
+      let(:params){ {sepval: nil, delimval: '|', calledby: mod} }
+
+      it 'returns expected' do
+        expect(result).to eq('|')
+        expect(mod).not_to receive(:warn)
+      end
+    end
+
+    context 'with sep and no delim' do
+      let(:warning) do
+        "#{Kiba::Extend.warning_label}:\n  Xform: `sep` parameter will "\
+          "be deprecated in a future release.\nTO FIX:\n  Change `sep` "\
+          "to `delim`"
+      end
+      let(:params){ {sepval: '|', delimval: nil, calledby: mod} }
+
+      it 'returns expected' do
+        expect(mod).to receive(:warn).with(warning)
+        expect(result).to eq('|')
+      end
+    end
+
+    context 'with sep and delim' do
+      let(:warning) do
+        "#{Kiba::Extend.warning_label}:\n  Xform: `sep` and `delim` "\
+          "parameters given. `delim` value used. `sep` value ignored. "\
+          "`sep` will be deprecated in a future release.\n"\
+          "TO FIX:\n  Remove `sep` param"
+      end
+      let(:params){ {sepval: ';', delimval: '|', calledby: mod} }
+
+      it 'returns expected' do
+        expect(mod).to receive(:warn).with(warning)
+        expect(result).to eq('|')
+      end
+    end
+
+    context 'without sep or delim' do
+      let(:params){ {sepval: nil, delimval: nil, calledby: mod} }
+
+      it 'returns expected' do
+        expect{result}.to raise_error(
+          ArgumentError,
+          "Xform: missing keyword: :delim"
+        )
+      end
+    end
+  end
+end
