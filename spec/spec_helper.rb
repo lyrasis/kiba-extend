1041a6888264a348eee47c7a20fb22f2559ba129
Kristina Spurgin <kristina.spurgin@lyrasis.org>
2022-11-03 20:11:03 -0400

    set rspec to run with random seed

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -22,6 +22,11 @@ end
 
 RSpec.configure do |config|
   config.include Helpers
+
+  # random but deterministic test order
+  config.order = :random
+  Kernel.srand config.seed
+
   # Enable flags like --only-failures and --next-failure
   config.example_status_persistence_file_path = '.rspec_status'
 
