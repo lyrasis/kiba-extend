67636890eb25ec368fda73be8d55f46be9bc7ce0
Kristina Spurgin <kristina.spurgin@lyrasis.org>
2023-02-17 15:01:43 -0500

    Fix redefined constant warnings when running rspec

--- spec/kiba/extend/destinations/json_array_spec.rb
+++ spec/kiba/extend/destinations/json_array_spec.rb
@@ -3,20 +3,21 @@
 require 'spec_helper'
 
 RSpec.describe Kiba::Extend::Destinations::JsonArray do
-  TEST_FILENAME = 'output.json'
+  before(:all){ @test_filename = 'output.json' }
+  let(:testfile){ @test_filename }
 
-  def run_job(input)
+  def run_job(input, output)
     job = Kiba.parse do
       source Kiba::Common::Sources::Enumerable, input
-      destination Kiba::Extend::Destinations::JsonArray, filename: TEST_FILENAME
+      destination Kiba::Extend::Destinations::JsonArray, filename: output
     end
 
     Kiba.run(job)
 
-    IO.read(TEST_FILENAME)
+    IO.read(output)
   end
 
-  after(:each){ File.delete(TEST_FILENAME) if File.exist?(TEST_FILENAME) }
+  after(:each){ File.delete(@test_filename) if File.exist?(@test_filename) }
 
   context 'when simplest data ever' do
     let(:input) do
@@ -29,7 +30,7 @@ RSpec.describe Kiba::Extend::Destinations::JsonArray do
       '[{"a":"and","b":"bat"},{"a":"apple","b":"board"}]'
     end
     it 'produces JSON as expected' do
-      expect(run_job(input)).to eq(expected)
+      expect(run_job(input, testfile)).to eq(expected)
     end
   end
 
@@ -58,13 +59,12 @@ RSpec.describe Kiba::Extend::Destinations::JsonArray do
          ]
        }]
     end
-    
+
     let(:expected) do
       '[{"Archival_Object__title":"Image of Bob and Sue","Archival_Object__dates":[{"Archival_Object__dates__begin":"1987","Archival_Object__dates__label":"other","Archival_Object__dates__date_type":"inclusive"},{"Archival_Object__dates__end":"1989","Archival_Object__dates__label":"other","Archival_Object__dates__date_type":"inclusive"}]},{"Archival_Object__title":"Audio recording of a horse","Archival_Object__subjects":["/Subjects/1","/Subjects/2"]}]'
     end
     it 'produces JSON as expected' do
-      expect(run_job(input)).to eq(expected)
+      expect(run_job(input, testfile)).to eq(expected)
     end
   end
 end
-
