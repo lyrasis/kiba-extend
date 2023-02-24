3c6b0ef286b6977d7a47eb1713c86bd042188448
Kristina Spurgin <kristina.spurgin@lyrasis.org>
2023-02-24 16:17:59 -0500

    Fix whitespace, line length

--- lib/kiba/extend/error.rb
+++ lib/kiba/extend/error.rb
@@ -2,7 +2,8 @@
 
 module Kiba
   module Extend
-    # Base for kiba-extend specific errors
+    # Base for kiba-extend specific errors, adding better identification of the
+    #   job from which the error is being raised
     class Error < StandardError
       def calling_job
         pre_base_job_initialize.reject{ |loc| loc.path['kiba-extend'] }.first
@@ -10,13 +11,15 @@ module Kiba
 
       private
 
-      # returns caller_locations stack prior to initialization of kiba-extend BaseJob initialization
+      # returns caller_locations stack prior to initialization of kiba-extend
+      #   BaseJob initialization
       def pre_base_job_initialize
         locs = caller_locations
         at_base_init = false
         until at_base_init
           loc = locs.shift
-          at_base_init = true if loc.path.end_with?('base_job.rb') && loc.label == 'initialize'
+          at_base_init = true if loc.path.end_with?('base_job.rb') &&
+            loc.label == 'initialize'
         end
         locs
       end
