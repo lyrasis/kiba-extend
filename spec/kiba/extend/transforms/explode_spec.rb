bf3e0bdf8d8b7be265eb4488d287f1084373ec52
Kristina Spurgin <kristina.spurgin@lyrasis.org>
2023-08-08 11:21:36 -0400

    Delete tests moved to separate file

--- spec/kiba/extend/transforms/explode_spec.rb
+++ spec/kiba/extend/transforms/explode_spec.rb
@@ -3,34 +3,6 @@
 require 'spec_helper'
 
 RSpec.describe Kiba::Extend::Transforms::Explode do
-  describe 'RowsFromMultivalField' do
-    rows = [
-      %w[id r1 r2],
-      ['001', 'a;b', 'foo;bar']
-    ]
-
-    before do
-      generate_csv(rows)
-    end
-    after do
-      File.delete(test_csv) if File.exist?(test_csv)
-      File.delete(lookup_csv) if File.exist?(lookup_csv)
-    end
-
-    context 'when given field :r1 and delim \';\'' do
-      it 'creates 2 rows with same :id and :r2 fields' do
-        expected = [
-          { id: '001', r1: 'a', r2: 'foo;bar' },
-          { id: '001', r1: 'b', r2: 'foo;bar' }
-        ]
-        result = execute_job(filename: test_csv,
-                             xform: Explode::RowsFromMultivalField,
-                             xformopt: { field: :r1, delim: ';' })
-        expect(result).to eq(expected)
-      end
-    end
-  end
-
   describe 'ColumnsRemappedInNewRows' do
     rows = [
       %w[f1 c1 f2 c2 season f3],
