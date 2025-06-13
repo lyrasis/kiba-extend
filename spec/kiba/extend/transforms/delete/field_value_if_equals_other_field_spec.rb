4ddee622cea00ffd5433e1f871b2ab8908bf0f68
Kristina Spurgin <kristina.spurgin@lyrasis.org>
2025-06-13 19:49:10 -0400

    test: Convert to yardspec

--- spec/kiba/extend/transforms/delete/field_value_if_equals_other_field_spec.rb
+++ spec/kiba/extend/transforms/delete/field_value_if_equals_other_field_spec.rb
@@ -3,112 +3,34 @@
 require "spec_helper"
 
 RSpec.describe Kiba::Extend::Transforms::Delete::FieldValueIfEqualsOtherField do
-  let(:accumulator) { [] }
-  let(:test_job) do
-    Helpers::TestJob.new(input: input, accumulator: accumulator,
-      transforms: transforms)
-  end
-  let(:result) { test_job.accumulator }
+  subject(:xform) { described_class.new(**params) }
 
-  let(:input) do
-    [
-      {del: "a", compare: "b"},
-      {del: "c", compare: "c"}
-    ]
+  let(:result) do
+    Kiba::StreamingRunner.transform_stream(input, xform)
+      .map { |row| row }
   end
 
-  let(:transforms) do
-    Kiba.job_segment do
-      transform Delete::FieldValueIfEqualsOtherField, delete: :del,
-        if_equal_to: :compare
-    end
-  end
-
-  let(:expected) do
-    [
-      {del: "a", compare: "b"},
-      {del: nil, compare: "c"}
-    ]
-  end
-
-  it "transforms as expected" do
-    expect(result).to eq(expected)
-  end
-
-  context "with grouped field(s)" do
-    context "where groups are even as expected" do
-      let(:input) do
-        [
-          {row: "1", del: "A;C;d;c;e", compare: "c", grpa: "y;x;w;u;v",
-           grpb: "e;f;g;h;i"},
-          {row: "2", del: "a;b;c", compare: "a;z;c", grpa: "d;e;f",
-           grpb: "g;h;i"},
-          {row: "3", del: "a", compare: "a;b", grpa: "d", grpb: "g"},
-          {row: "4", del: "a", compare: "b", grpa: "z", grpb: "q"},
-          {row: "5", del: "a", compare: "a", grpa: "z", grpb: "q"}
-        ]
-      end
-
-      let(:transforms) do
-        Kiba.job_segment do
-          transform Delete::FieldValueIfEqualsOtherField,
-            delete: :del,
-            if_equal_to: :compare,
-            multival: true,
-            delim: ";",
-            grouped_fields: %i[grpa grpb],
-            casesensitive: false
-        end
-      end
-
-      let(:expected) do
-        [
-          {row: "1", del: "A;d;e", compare: "c", grpa: "y;w;v", grpb: "e;g;i"},
-          {row: "2", del: "b", compare: "a;z;c", grpa: "e", grpb: "h"},
-          {row: "3", del: nil, compare: "a;b", grpa: nil, grpb: nil},
-          {row: "4", del: "a", compare: "b", grpa: "z", grpb: "q"},
-          {row: "5", del: nil, compare: "a", grpa: nil, grpb: nil}
-        ]
-      end
-
-      it "transforms as expected" do
-        expect(result).to eq(expected)
-      end
+  context "with ragged grouped fields" do
+    let(:input) do
+      [
+        {del: "A;C;d;e;c", compare: "c", grpa: "y;x;w;u", grpb: "e;f;g;h;i"}
+      ]
     end
 
-    context "where groups are ragged" do
-      let(:input) do
-        [
-          {del: "A;C;d;e;c", compare: "c", grpa: "y;x;w;u", grpb: "e;f;g;h;i"}
-        ]
-      end
+    let(:params) do
+      {
+        delete: :del,
+        if_equal_to: :compare,
+        multival: true,
+        delim: ";",
+        grouped_fields: %i[grpa grpb],
+        casesensitive: false
+      }
+    end
 
-      let(:transforms) do
-        Kiba.job_segment do
-          transform Delete::FieldValueIfEqualsOtherField,
-            delete: :del,
-            if_equal_to: :compare,
-            multival: true,
-            delim: ";",
-            grouped_fields: %i[grpa grpb],
-            casesensitive: false
-        end
-      end
-
-      let(:expected) do
-        [
-          {del: "A;d;e", compare: "c", grpa: "y;w;u", grpb: "e;g;h"}
-        ]
-      end
-
-      it "transforms as expected" do
-        expect(result).to eq(expected)
-      end
-
-      it "outputs warning to STDOUT" do
-        msg = /KIBA WARNING: One or more grouped fields.*/
-        expect { result }.to output(msg).to_stdout
-      end
+    it "outputs warning to STDOUT" do
+      msg = /KIBA WARNING: One or more grouped fields.*/
+      expect { result }.to output(msg).to_stdout
     end
   end
 end
