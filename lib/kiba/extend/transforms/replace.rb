7dffec236ac389cb6106b5ee2eecaa21e0aeca87
Kristina Spurgin <kristina.spurgin@lyrasis.org>
2025-07-23 12:16:51 -0400

    style: Fix Layout::LineLength

--- lib/kiba/extend/transforms/replace.rb
+++ lib/kiba/extend/transforms/replace.rb
@@ -1,17 +1,15 @@
 # frozen_string_literal: true
 
-# rubocop:todo Layout/LineLength
-
 module Kiba
   module Extend
     module Transforms
       # Transformations that perform replacements of full field values
       #
-      # For finding/replacing within field values, see {Clean::RegexpFindReplaceFieldVals}
+      # For finding/replacing within field values, see
+      #   {Clean::RegexpFindReplaceFieldVals}
       module Replace
         ::Replace = Kiba::Extend::Transforms::Replace
       end
     end
   end
 end
-# rubocop:enable Layout/LineLength
