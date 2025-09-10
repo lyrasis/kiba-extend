dcac9bb7ce9bda1bea622d19f914eb276c20f89a
Kristina Spurgin <kristina.spurgin@lyrasis.org>
2025-09-10 12:42:47 -0400

    test: Convert to yardspec

--- lib/kiba/extend/transforms/explode/columns_remapped_in_new_rows.rb
+++ lib/kiba/extend/transforms/explode/columns_remapped_in_new_rows.rb
@@ -4,54 +4,63 @@ module Kiba
   module Extend
     module Transforms
       module Explode
-        # This one is hard to succintly describe! Use it if you have specific fields grouped together in a row, and you want to abstract them so the fields are broader, and you reduce the number of fields.
+        # This one is hard to succintly describe! Use it if you have
+        #   specific fields grouped together in a row, and you want to
+        #   abstract them so the fields are broader, and you reduce
+        #   the number of fields.
         #
-        # In the example, 3 fields in each row represent fruit names and 2 fields in each row represent colors. We want more rows, but each row should have only one fruit column and color column.
+        # In the example, 3 fields in each row represent fruit names
+        #   and 2 fields in each row represent colors. We want more
+        #   rows, but each row should have only one fruit column and
+        #   color column.
         #
-        # # Example
+        # Things to notice in example:
         #
-        # Input table:
-        #
-        # ~~~
-        # | f1           | c1          | f2         | c2    | season | f3          |
-        # |--------------+-------------+------------+-------+--------+-------------|
-        # | strawberry   | red         | blueberry  | blue  | spring | cherry      |
-        # | fig;honeydew | brown;green | watermelon | green | summer | nil         |
-        # | nil          | nil         | nil        | nil   | winter | grapefruit  |
-        # | nil          | nil         | nil        | nil   | autumn | nil         |
-        # ~~~
-        #
-        # Used in pipeline as:
-        #
-        # ~~~
-        # transform Explode::ColumnsRemappedInNewRows,
-        #   remap_groups: [
-        #     [:f1, :c1],
-        #     [:f2, :c2],
-        #     [:f3]
-        #   ],
-        #  map_to: [:fruit, :color]
-        # ~~~
-        #
-        # Results in:
-        #
-        # ~~~
-        # | fruit        | color       | season |
-        # |--------------+-------------+--------|
-        # | strawberry   | red         | spring |
-        # | blueberry    | blue        | spring |
-        # | cherry       | nil         | spring |
-        # | fig;honeydew | brown;green | summer |
-        # | watermelon   | green       | summer |
-        # | grapefruit   | nil         | winter |
-        # | nil          | nil         | autumn |
-        # ~~~
-        #
-        # ## Things to notice
-        #
-        # * The fact that some field values are multivalued is completely ignored
+        # * The fact that some field values are multivalued is completely
+        #   ignored
         # * If all the values for a given remap group are blank, no row is added
-        # * Values in fields not included in `remap_groups` are copied to every row created
+        # * Values in fields not included in `remap_groups` are copied to every
+        #   row created
+        #
+        # @example With defaults
+        #   # Used in pipeline as:
+        #   # transform Explode::ColumnsRemappedInNewRows,
+        #   #   remap_groups: [
+        #   #     [:f1, :c1],
+        #   #     [:f2, :c2],
+        #   #     [:f3]
+        #   #   ],
+        #   #   map_to: [:fruit, :color]
+        #   xform = Explode::ColumnsRemappedInNewRows.new(
+        #     remap_groups: [
+        #       [:f1, :c1],
+        #       [:f2, :c2],
+        #       [:f3]
+        #     ],
+        #     map_to: [:fruit, :color]
+        #   )
+        #   input = [
+        #     {f1: "strawberry", c1: "red", f2: "blueberry", c2: "blue",
+        #       season: "spring", f3: "cherry"},
+        #     {f1: "fig;honeydew", c1: "brown;green", f2: "watermelon",
+        #       c2: "green", season: "summer", f3: nil},
+        #     {f1: nil, c2: nil, f2: nil, c2: nil, season: "winter",
+        #       f3: "grapefruit"},
+        #     {f1: nil, c2: nil, f2: nil, c2: nil, season: "autumn",
+        #       f3: nil}
+        #   ]
+        #   result = Kiba::StreamingRunner.transform_stream(input, xform)
+        #     .map{ |row| row }
+        #   expected = [
+        #     {fruit: "strawberry", color: "red", season: "spring"},
+        #     {fruit: "blueberry", color: "blue", season: "spring"},
+        #     {fruit: "cherry", color: nil, season: "spring"},
+        #     {fruit: "fig;honeydew", color: "brown;green", season: "summer"},
+        #     {fruit: "watermelon", color: "green", season: "summer"},
+        #     {fruit: "grapefruit", color: nil, season: "winter"},
+        #     {fruit: nil, color: nil, season: "autumn"}
+        #   ]
+        #   expect(result).to eq(expected)
         class ColumnsRemappedInNewRows
           # @param remap_groups [Array(Array(Symbol))] The existing field groups that should be
           def initialize(remap_groups:, map_to:)
--- lib/kiba/extend/transforms/explode/field_values_to_new_rows.rb
+++ lib/kiba/extend/transforms/explode/field_values_to_new_rows.rb
@@ -4,6 +4,211 @@ module Kiba
   module Extend
     module Transforms
       module Explode
+        # @example Multival, without keeping nil or empty
+        #   # Used in pipeline as:
+        #   # transform Explode::FieldValuesToNewRows,
+        #   #   fields: %i[child parent],
+        #   #   target: :val,
+        #   #   multival: true,
+        #   #   sep: ";"
+        #   xform = Explode::FieldValuesToNewRows.new(
+        #     fields: %i[child parent],
+        #     target: :val,
+        #     multival: true,
+        #     sep: ";"
+        #   )
+        #   input = [
+        #     {id: 1, child: "a;b", parent: "c;d"},
+        #     {id: 2, child: "a", parent: "b"},
+        #     {id: 3, child: "", parent: "q"},
+        #     {id: 4, child: "n", parent: nil},
+        #     {id: 5, child: "", parent: nil},
+        #     {id: 6, child: "p;", parent: ";z"},
+        #     {id: 7, child: "m;;n", parent: "s"}
+        #   ]
+        #   result = Kiba::StreamingRunner.transform_stream(input, xform)
+        #     .map{ |row| row }
+        #   expected = [
+        #     {id: 1, val: "a"},
+        #     {id: 1, val: "b"},
+        #     {id: 1, val: "c"},
+        #     {id: 1, val: "d"},
+        #     {id: 2, val: "a"},
+        #     {id: 2, val: "b"},
+        #     {id: 3, val: "q"},
+        #     {id: 4, val: "n"},
+        #     {id: 6, val: "p"},
+        #     {id: 6, val: "z"},
+        #     {id: 7, val: "m"},
+        #     {id: 7, val: "n"},
+        #     {id: 7, val: "s"}
+        #   ]
+        #   expect(result).to eq(expected)
+        # @example Multival, keeping nil
+        #   # Used in pipeline as:
+        #   # transform Explode::FieldValuesToNewRows,
+        #   #   fields: %i[child parent],
+        #   #   target: :val,
+        #   #   multival: true,
+        #   #   sep: ";",
+        #   #   keep_nil: true
+        #   xform = Explode::FieldValuesToNewRows.new(
+        #     fields: %i[child parent],
+        #     target: :val,
+        #     multival: true,
+        #     sep: ";",
+        #     keep_nil: true
+        #   )
+        #   input = [
+        #     {id: 1, child: "a;b", parent: "c;d"},
+        #     {id: 2, child: "a", parent: "b"},
+        #     {id: 3, child: "", parent: "q"},
+        #     {id: 4, child: "n", parent: nil},
+        #     {id: 5, child: "", parent: nil},
+        #     {id: 6, child: "p;", parent: ";z"},
+        #     {id: 7, child: "m;;n", parent: "s"}
+        #   ]
+        #   result = Kiba::StreamingRunner.transform_stream(input, xform)
+        #     .map{ |row| row }
+        #   expected = [
+        #     {id: 1, val: "a"},
+        #     {id: 1, val: "b"},
+        #     {id: 1, val: "c"},
+        #     {id: 1, val: "d"},
+        #     {id: 2, val: "a"},
+        #     {id: 2, val: "b"},
+        #     {id: 3, val: "q"},
+        #     {id: 4, val: "n"},
+        #     {id: 4, val: nil},
+        #     {id: 5, val: nil},
+        #     {id: 6, val: "p"},
+        #     {id: 6, val: "z"},
+        #     {id: 7, val: "m"},
+        #     {id: 7, val: "n"},
+        #     {id: 7, val: "s"}
+        #   ]
+        #   expect(result).to eq(expected)
+        # @example Multival, keeping empty
+        #   # Used in pipeline as:
+        #   # transform Explode::FieldValuesToNewRows,
+        #   #   fields: %i[child parent],
+        #   #   target: :val,
+        #   #   multival: true,
+        #   #   sep: ";",
+        #   #   keep_empty: true
+        #   xform = Explode::FieldValuesToNewRows.new(
+        #     fields: %i[child parent],
+        #     target: :val,
+        #     multival: true,
+        #     sep: ";",
+        #     keep_empty: true
+        #   )
+        #   input = [
+        #     {id: 1, child: "a;b", parent: "c;d"},
+        #     {id: 2, child: "a", parent: "b"},
+        #     {id: 3, child: "", parent: "q"},
+        #     {id: 4, child: "n", parent: nil},
+        #     {id: 5, child: "", parent: nil},
+        #     {id: 6, child: "p;", parent: ";z"},
+        #     {id: 7, child: "m;;n", parent: "s"}
+        #   ]
+        #   result = Kiba::StreamingRunner.transform_stream(input, xform)
+        #     .map{ |row| row }
+        #   expected = [
+        #     {id: 1, val: "a"},
+        #     {id: 1, val: "b"},
+        #     {id: 1, val: "c"},
+        #     {id: 1, val: "d"},
+        #     {id: 2, val: "a"},
+        #     {id: 2, val: "b"},
+        #     {id: 3, val: ""},
+        #     {id: 3, val: "q"},
+        #     {id: 4, val: "n"},
+        #     {id: 5, val: ""},
+        #     {id: 6, val: "p"},
+        #     {id: 6, val: ""},
+        #     {id: 6, val: ""},
+        #     {id: 6, val: "z"},
+        #     {id: 7, val: "m"},
+        #     {id: 7, val: ""},
+        #     {id: 7, val: "n"},
+        #     {id: 7, val: "s"}
+        #   ]
+        #   expect(result).to eq(expected)
+        # @example Single val, without keeping nil or empty
+        #   # Used in pipeline as:
+        #   # transform Explode::FieldValuesToNewRows,
+        #   #   fields: %i[child parent],
+        #   #   target: :val,
+        #   xform = Explode::FieldValuesToNewRows.new(
+        #     fields: %i[child parent],
+        #     target: :val,
+        #   )
+        #   input = [
+        #     {id: 1, child: "a;b", parent: "c;d"},
+        #     {id: 2, child: "a", parent: "b"},
+        #     {id: 3, child: "", parent: "q"},
+        #     {id: 4, child: "n", parent: nil},
+        #     {id: 5, child: "", parent: nil},
+        #     {id: 6, child: "p;", parent: ";z"},
+        #     {id: 7, child: "m;;n", parent: "s"}
+        #   ]
+        #   result = Kiba::StreamingRunner.transform_stream(input, xform)
+        #     .map{ |row| row }
+        #   expected = [
+        #     {id: 1, val: "a;b"},
+        #     {id: 1, val: "c;d"},
+        #     {id: 2, val: "a"},
+        #     {id: 2, val: "b"},
+        #     {id: 3, val: "q"},
+        #     {id: 4, val: "n"},
+        #     {id: 6, val: "p;"},
+        #     {id: 6, val: ";z"},
+        #     {id: 7, val: "m;;n"},
+        #     {id: 7, val: "s"}
+        #   ]
+        #   expect(result).to eq(expected)
+        # @example Single val, keeping nil and empty
+        #   # Used in pipeline as:
+        #   # transform Explode::FieldValuesToNewRows,
+        #   #   fields: %i[child parent],
+        #   #   target: :val,
+        #   #   keep_nil: true,
+        #   #   keep_empty: true
+        #   xform = Explode::FieldValuesToNewRows.new(
+        #     fields: %i[child parent],
+        #     target: :val,
+        #     keep_nil: true,
+        #     keep_empty: true
+        #   )
+        #   input = [
+        #     {id: 1, child: "a;b", parent: "c;d"},
+        #     {id: 2, child: "a", parent: "b"},
+        #     {id: 3, child: "", parent: "q"},
+        #     {id: 4, child: "n", parent: nil},
+        #     {id: 5, child: "", parent: nil},
+        #     {id: 6, child: "p;", parent: ";z"},
+        #     {id: 7, child: "m;;n", parent: "s"}
+        #   ]
+        #   result = Kiba::StreamingRunner.transform_stream(input, xform)
+        #     .map{ |row| row }
+        #   expected = [
+        #     {id: 1, val: "a;b"},
+        #     {id: 1, val: "c;d"},
+        #     {id: 2, val: "a"},
+        #     {id: 2, val: "b"},
+        #     {id: 3, val: ""},
+        #     {id: 3, val: "q"},
+        #     {id: 4, val: "n"},
+        #     {id: 4, val: nil},
+        #     {id: 5, val: ""},
+        #     {id: 5, val: nil},
+        #     {id: 6, val: "p;"},
+        #     {id: 6, val: ";z"},
+        #     {id: 7, val: "m;;n"},
+        #     {id: 7, val: "s"}
+        #   ]
+        #   expect(result).to eq(expected)
         class FieldValuesToNewRows
           def initialize(target:, fields: [], multival: false, sep: " ",
             keep_nil: false, keep_empty: false)
