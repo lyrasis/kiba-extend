018cb45bcca76ee0a143e4da895e43b3188619e0
Kristina Spurgin <kristina.spurgin@lyrasis.org>
2025-12-09 18:24:30 -0500

    feat: Add Lookup.from_job method

--- lib/kiba/extend/utils/lookup.rb
+++ lib/kiba/extend/utils/lookup.rb
@@ -22,6 +22,25 @@ module Kiba
           lookup.hash
         end
 
+        # Creates hash from a registered job key outside of the context of job
+        #   files setup
+        # @param jobkey [Symbol] registry entry key for job with namespace
+        # @param lookup_on [nil,Symbol] field name on which rows are
+        #   grouped/looked up; will use value defined for registry entry if not
+        #   provided
+        # @param csvopt [Hash] options for reading/parsing CSV
+        # @return [Hash]
+        def from_job(jobkey:, lookup_on: nil, csvopt: Kiba::Extend.csvopts)
+          entry = Kiba::Extend::Registry.entry_for(jobkey)
+          path = entry.path
+          lkup = lookup_on || entry.lookup_on
+          unless lkup
+            fail Kiba::Extend::NoLookupOnError.new(jobkey, "Lookup.from_job")
+          end
+
+          csv_to_hash(file: path, keycolumn: lkup, csvopt: csvopt)
+        end
+
         # Turns any Enumerable where each item is a record/row hash
         #  into an expected lookup hash via Utils::LookupHash
         # @param enum [#each<Hash>] rows/records to turn into the lookup source
