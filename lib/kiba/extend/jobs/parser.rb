9a5054c6b1b58bf1029a6ea8451af8695cce641f
Kristina Spurgin <kristina.spurgin@lyrasis.org>
2026-07-24 18:43:35 -0400

    docs: Add yardoc

--- lib/kiba/extend/jobs/parser.rb
+++ lib/kiba/extend/jobs/parser.rb
@@ -4,6 +4,11 @@ module Kiba
   module Extend
     module Jobs
       module Parser
+        # Evals job_segment Procs defined for a job in the job context
+        # @param control [Kiba::Control]
+        # @param context [Kiba::Context]
+        # @param job_segments [Array<Proc>]
+        # @return [Kiba::Control]
         def parse_job(control, context, *job_segments)
           job_segments = job_segments.flatten
           job_segments.compact.each do |segment|
